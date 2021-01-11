807960341c12b74594d220e3e39bea98a8ab4ca4
BethPech <colnze17-94@mail.ru>
2021-01-11 17:49:45 +0300

    Some mistakes were corrected

--- src/test/java/ru/netology/service/CashbackTest.java
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -10,27 +10,30 @@ public class CashbackTest {
         Cashback service = new Cashback();
         int actual = service.remain(900);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
     public void shouldCountHowMuchAddIfMore() {
         Cashback service = new Cashback();
         int actual = service.remain(1500);
         int expected = 500;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
     public void shouldCountHowMuchAddIfEqual() {
         Cashback service = new Cashback();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
     public void shouldCountHowMuchAddIfZero() {
         Cashback service = new Cashback();
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
